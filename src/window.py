ead67efc035212f8547226a92033a4e7c5053c7a
mlamb <h.max.lamb@gmail.com>
2024-09-12 14:45:55 -0400

    2: working on window class

--- src/window.py
+++ src/window.py
@@ -2,7 +2,13 @@ import tkinter import TK, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__root = tkinter.TK()
+        self.__root.title("Mazesolver") 
+        self.__root.Canvas(self.__root, bg="white", height=height, width=width)
+        self.__root.pack(fill="both", expand=True)
+        self.__running = False
         
+
+
+
 
