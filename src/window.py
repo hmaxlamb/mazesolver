a3bde426788f3f45e2ac56519a06498ccb4addb5
Max Lamb <h.max.lamb@gmail.com>
2024-09-27 14:02:40 -0400

    3: finished base window

--- src/window.py
+++ src/window.py
@@ -1,14 +1,22 @@
-import tkinter import TK, BOTH, Canvas
+from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.__root = tkinter.TK()
+        self.__root = Tk()
         self.__root.title("Mazesolver") 
-        self.__root.Canvas(self.__root, bg="white", height=height, width=width)
-        self.__root.pack(fill="both", expand=True)
+        self.canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.canvas.pack(fill="both", expand=1)
         self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wati_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
         
-
-
-
-
+    def close(self):
+        self.__running = False   
