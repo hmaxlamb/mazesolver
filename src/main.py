6e85c55bb42bfda613d92ff5e5db68bcdd9bd757
Max Lamb <h.max.lamb@gmail.com>
2024-10-01 08:38:41 -0400

    4: line_methods

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
-from window import Window
+from window import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+
+    p1 = Point(43, 87)
+    p2 = Point(641, 450)
+
+    l1 = Line(p1, p2)
+
+    win.draw_line(l1, "red")
+
     win.wati_for_close()
+
 
 main()
--- src/window.py
+++ src/window.py
@@ -20,3 +20,19 @@ class Window:
         
     def close(self):
         self.__running = False   
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas ,fill_color)
+
+class Point:
+    def __init__(self, xcord, ycord):
+        self.x = xcord
+        self.y = ycord
+
+class Line:
+    def __init__(self, point1, point2):
+        self.p1 = point1
+        self.p2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
